db51e9eb3a4d968aeef48b31d9047fed249306b0
Haley Taft <haleytaft@comcast.net>
2020-09-16 23:06:59 -0400

    webpage navigation progress, capstone checkin2 progress

--- selenium_practice.py
+++ selenium_practice.py
@@ -8,16 +8,13 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
-# options = Options()
-# options.headless = True
 DRIVER_PATH = '/Users/haleytaft/Downloads/chromedriver'
 
 
 driver = webdriver.Chrome( executable_path=DRIVER_PATH) #options=options,
 driver.get("https://www.chewy.com/b/toys-315")
-# assert "Dog Supplies: Best Dog & Puppy Products (Free Shipping)" in driver.title
 
-# To first just look at Chew Toys
+# To first just look at CHEW TOYS
 chew_toys_link = driver.find_element_by_link_text('Chew Toys')
 chew_toys_link.click()
 
@@ -51,11 +48,13 @@ try:
     check = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.LINK_TEXT, "Chew Toys")))
     driver.back() # back to chew toys
+    # DONE WITH CHEW TOYS
 
-    driver.back() # back to toys
+    # back to toys
+    driver.back()
     # driver.back()
 
-    # Looking at Plush Toys
+    # Looking at PLUSH TOYS
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.LINK_TEXT, "Plush Toys")))
     element.click()
@@ -64,7 +63,7 @@ try:
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.LINK_TEXT, "Stuffed Toys")))
     element.click()
-    # To look at the tough chew toys -- NEED TO FIGURE OUT HOW TO ACCESS THEM
+    # To look at the stuffed plush toys -- NEED TO FIGURE OUT HOW TO ACCESS THEM
     check = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.LINK_TEXT, "Plush Toys")))
     # To get back to plush toys
@@ -74,67 +73,61 @@ try:
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.LINK_TEXT, "Unstuffed Toys")))
     element.click()
-    # To look at the tough chew toys -- NEED TO FIGURE OUT HOW TO ACCESS THEM
+    # To look at the unstuffed plush toys -- NEED TO FIGURE OUT HOW TO ACCESS THEM
     check = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.LINK_TEXT, "Plush Toys")))
     # To get back to plush toys
     driver.back()
+    # DONE WITH PLUSH TOYS
 
     # Back to toys
     driver.back()
 
-    # articles = driver.find_elements_by_class_name('product-holder js-tracked-product cw-card cw-card-hover')
+    # Now Looking at FETCH TOYS
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Fetch Toys")))
+    element.click()
 
-    # div = main.find_elements_by_id("tns1")
-    # print(div)
-    # for a in divs:
-    #     header = div.find_element_by_class_name("")
+    # Looking a the Balls toys
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Balls")))
+    element.click()
+    # To look at the ball fetch toys -- NEED TO FIGURE OUT HOW TO ACCESS THEM
+    check = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Fetch Toys")))
+    # To get back to fetch toys
+    driver.back()
 
+    # Looking a the Discs toys
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Discs")))
+    element.click()
+    # To look at the disc fetch toys -- NEED TO FIGURE OUT HOW TO ACCESS THEM
+    check = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Fetch Toys")))
+    # To get back to fetch toys
+    driver.back()
+
+    # Looking a the Launcher toys
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Launchers")))
+    element.click()
+    # To look at the launcher fetch toys -- NEED TO FIGURE OUT HOW TO ACCESS THEM
+    check = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Fetch Toys")))
+    # To get back to fetch toys
+    driver.back()
+
+    # Looking a the Stick toys
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Sticks")))
+    element.click()
+    # To look at the stick fetch toys -- NEED TO FIGURE OUT HOW TO ACCESS THEM
+    check = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Fetch Toys")))
+    # To get back to fetch toys
+    driver.back()
+    # DONE WITH FETCH TOYS
 
 except:
     driver.quit()
-    # print(main.text)
-
-    # div_department = main.find_element_by_class_name('department')
-
-    # print(div_department.text)
-    # div_container = div_department.find_element_by_class_name('container')
-    # print(div_container.text)
-
-    # aside = div_department.find_element_by_class_name('dept-sliders')
-
-    # print("hi")
-
-    # ul = div_department.find_element_by_tag_name('ul')
-
-    # print("hello")
-
-    # lis = ul.find_elements_by_tag_name('li')
-
-    # print(lis[2])
-    # li = lis[2]
-
-    # toys = ul.find_elements_by_tag_name("a")
-    # print(toys[1])
-
-    # toys_link = driver.find_element_by_link_text("Toys")
-    # print(toys_link)
-    # toys_link.click()
-    # time.sleep(5)
-
-    # for il in
-    # toys_link = toys.find_element_by_link_text('/b/toys-315')
-    # print(toys_link)
-
-# toys = driv er.find_element_by_tag_name('a')
-# toys_link = driver.find_element_by_partial_link_text('/b/toys-315')
-# driver.get(toys_link)
-# print(driver.title)
-
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-
-# assert "No results found." not in driver.page_source
-# time.sleep(5)
-# driver.close()
